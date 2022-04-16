409b3469ae90d9c7ac9482e5a7062f4f3baa3bcd
kaustubh <kaustubh.education1@gmail.com>
2022-04-16 16:35:58 +0530

    Finally created and hosted the application on aws cloud. Many changes were done to the existing file, but was worth it.

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -89,9 +89,9 @@ export const listBooks = /* GraphQL */ `
         author
         featured
         price
-        # orders {
-        #   nextToken
-        # }
+        orders {
+          nextToken
+        }
         createdAt
         updatedAt
       }
--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -5,7 +5,7 @@ import { Elements } from '@stripe/react-stripe-js';
 import CheckoutForm from "../components/CheckoutForm";
 
 const Checkout = () => {
-    const stripePromise = loadStripe('<stripe_public_key>');
+    const stripePromise = loadStripe('pk_test_51Kp5egBGuDR7GKdyPiL49ILlmTGbPrILeyiUJ1qj2bY8lFmxu2xNePlpSlrrYwR1qDyzWoHgzx0l0sHTCOWj1cgV0029aZveU3');
 
     return (
         <section className="checkout-wrapper">
